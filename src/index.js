84f4632dd17dc4ff6ae33859e244961e8e669b8e
Wow-Its-Ian <wallsianh@gmail.com>
2022-03-14 01:41:38 -0400

    basic raycasting

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ for (let i = 0; i < interactives.length; i++) {
   const element = interactives[i];
   console.log('element: ', element);
   element.addEventListener('click', () => console.log('CLICK'));
-  element.addEventListener('raycaster-intersected', () => console.log('RAYCASTER'));
+  element.addEventListener('raycaster-intersected', () => element.setAttribute('color', 'red'));
+  element.addEventListener('raycaster-intersected-cleared', () => element.setAttribute('color', 'white'));
 }
