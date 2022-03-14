2516bfcf337a2a22aa19d0fdc474a7fc8530fb9c
Wow-Its-Ian <wallsianh@gmail.com>
2022-03-14 13:22:36 -0400

    removed console.logs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const interactives = document.getElementsByClassName('interactive');
 
 for (let i = 0; i < interactives.length; i++) {
   const element = interactives[i];
-  console.log('element: ', element);
+
   element.addEventListener('click', () => console.log('CLICK'));
   element.addEventListener('raycaster-intersected', () => element.setAttribute('color', 'red'));
   element.addEventListener('raycaster-intersected-cleared', () => element.setAttribute('color', 'white'));
